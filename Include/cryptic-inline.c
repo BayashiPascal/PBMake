6a4bb9b95001bc36090add7d11a3e599775e1c90
BayashiPascal <bayashipascal@gmail.com>
2020-05-24 18:12:36 +0900

    Regenerate the archive

--- Include/cryptic-inline.c
+++ Include/cryptic-inline.c
@@ -2,22 +2,18 @@
 
 // ================ Functions implementation ====================
 
-// Static constructor for a Feistel cypher,
+// Static constructor for a Feistel cipher,
 // 'keys' is a GSet of null terminated strings, all the same size
-// 'fun' is the cyphering function of the form
+// 'fun' is the ciphering function of the form
 // void (*fun)(char* src, char* dest, char* key, unsigned long len)
 // 'src', 'dest' have same length 'len'
 // 'key' may be of any length
 #if BUILDMODE != 0
 static inline
 #endif
-FeistelCyphering FeistelCypheringCreateStatic(
+FeistelCiphering FeistelCipheringCreateStatic(
   GSetStr* keys,
-  void (*fun)(
-    unsigned char*,
-    unsigned char*,
-    unsigned char*,
-    unsigned long)) {
+      void FUN_CIPHER) {
 
 #if BUILDMODE == 0
 
@@ -43,15 +39,254 @@ FeistelCyphering FeistelCypheringCreateStatic(
 
 #endif
 
-  // Declare a FeistelCyphering and set the properties
-  FeistelCyphering c = {
+  // Declare a FeistelCiphering and set the properties
+  FeistelCiphering c = {
 
     .keys = keys,
-    .fun = fun
+    .fun = fun,
+    .mode = DEFAULT_OP_MODE,
+    .initVector = NULL,
+    .streamBuffer = NULL,
+    .counter = 0
 
   };
 
-  // Return the FeistelCyphering
+  // Return the FeistelCiphering
   return c;
+
+}
+
+// Get the operating mode of the FeistelCiphering 'that'
+#if BUILDMODE != 0
+static inline
+#endif
+FeistelCipheringOpMode FeistelCipheringGetOpMode(
+  const FeistelCiphering* const that) {
+
+#if BUILDMODE == 0
+
+  if (that == NULL) {
+
+    CrypticErr->_type = PBErrTypeNullPointer;
+    sprintf(
+      CrypticErr->_msg,
+      "'that' is null");
+    PBErrCatch(CrypticErr);
+
+  }
+
+#endif
+
+  // Return the operating mode
+  return that->mode;
+
+}
+
+// Set the operating mode of the FeistelCiphering 'that' to 'mode'
+#if BUILDMODE != 0
+static inline
+#endif
+void FeistelCipheringSetOpMode(
+  FeistelCiphering* const that,
+   FeistelCipheringOpMode mode) {
+
+#if BUILDMODE == 0
+
+  if (that == NULL) {
+
+    CrypticErr->_type = PBErrTypeNullPointer;
+    sprintf(
+      CrypticErr->_msg,
+      "'that' is null");
+    PBErrCatch(CrypticErr);
+
+  }
+
+#endif
+
+  // Set the operating mode
+  that->mode = mode;
+
+}
+
+// Get the initialisation vector of the FeistelCiphering 'that'
+#if BUILDMODE != 0
+static inline
+#endif
+const unsigned char* FeistelCipheringGetInitVec(
+  const FeistelCiphering* const that) {
+
+#if BUILDMODE == 0
+
+  if (that == NULL) {
+
+    CrypticErr->_type = PBErrTypeNullPointer;
+    sprintf(
+      CrypticErr->_msg,
+      "'that' is null");
+    PBErrCatch(CrypticErr);
+
+  }
+
+#endif
+
+  // Return the initialising vector
+  return that->initVector;
+
+}
+
+// Set the initialisation vector of the FeistelCiphering 'that'
+// to 'initVec'
+// Allocate extra memory to append the counter at the end of the
+// initialisation vector if the operation mode is CTR
+#if BUILDMODE != 0
+static inline
+#endif
+void FeistelCipheringSetInitVec(
+     FeistelCiphering* const that,
+  const unsigned char* const initVec) {
+
+#if BUILDMODE == 0
+
+  if (that == NULL) {
+
+    CrypticErr->_type = PBErrTypeNullPointer;
+    sprintf(
+      CrypticErr->_msg,
+      "'that' is null");
+    PBErrCatch(CrypticErr);
+
+  }
+
+#endif
+
+  // Free memory if necessary
+  if (that->initVector != NULL) {
+
+    free(that->initVector);
+
+  }
+
+  switch (FeistelCipheringGetOpMode(that)) {
+
+    case FeistelCipheringOpMode_CBC:
+
+      // Copy the initialising vector
+      that->initVector = (unsigned char*)strdup((char*)initVec);
+      break;
+
+    case FeistelCipheringOpMode_CTR:
+
+      // Allocate memory
+      that->initVector =
+        (unsigned char*)malloc(
+          strlen((char*)initVec) + 1 + sizeof(that->counter));
+
+      // Set the last byte to null
+      unsigned long posLastByte =
+        strlen((char*)initVec) + sizeof(that->counter);
+      that->initVector[posLastByte] = '\0';
+
+      // Copy the initialising vector
+      memcpy(
+        that->initVector,
+        initVec,
+        strlen((char*)initVec));
+
+      break;
+
+    default:
+
+      break;
+
+    }
+
+}
+
+// Initialise the stream encoding/decoding of the FeistelCiphering 'that'
+// with the initialization vector 'initVec'
+#if BUILDMODE != 0
+static inline
+#endif
+void FeistelCipheringInitStream(
+     FeistelCiphering* const that,
+  const unsigned char* const initVec) {
+
+#if BUILDMODE == 0
+
+  if (that == NULL) {
+
+    CrypticErr->_type = PBErrTypeNullPointer;
+    sprintf(
+      CrypticErr->_msg,
+      "'that' is null");
+    PBErrCatch(CrypticErr);
+
+  }
+
+#endif
+
+  // Initialise the properties used to encode/decode the stream
+  that->counter = 0;
+  FeistelCipheringSetInitVec(
+    that,
+    initVec);
+  if (that->streamBuffer != NULL) {
+
+    free(that->streamBuffer);
+
+  }
+
+  that->streamBuffer = NULL;
+
+}
+
+// Get the required size of the initialisation vector for the
+// FeistelCiphering 'that' for messages of length 'lenMsg'
+#if BUILDMODE != 0
+static inline
+#endif
+unsigned long FeistelCipheringGetReqSizeInitVec(
+  const FeistelCiphering* const that,
+            const unsigned long lenMsg) {
+
+#if BUILDMODE == 0
+
+  if (that == NULL) {
+
+    CrypticErr->_type = PBErrTypeNullPointer;
+    sprintf(
+      CrypticErr->_msg,
+      "'that' is null");
+    PBErrCatch(CrypticErr);
+
+  }
+
+#endif
+
+  // Declare a variable to memorize the size
+  unsigned long size = 0;
+
+  // Return the size of initialising vector
+  switch (FeistelCipheringGetOpMode(that)) {
+
+    case FeistelCipheringOpMode_CBC:
+
+      size = lenMsg;
+      break;
+
+    case FeistelCipheringOpMode_CTR:
+
+      size = lenMsg - sizeof(that->counter);
+      break;
+
+    default:
+
+      size = 0;
+
+  }
+
+  // Return the size of initialising vector
+  return size;
 
 }
--- Include/cryptic.h
+++ Include/cryptic.h
@@ -11,65 +11,167 @@
 
 // ================= Define ==================
 
+#define FUN_CIPHER \
+  (*fun)( \
+    unsigned char*, \
+    unsigned char*, \
+    unsigned char*, \
+    unsigned long)
+
+#define DEFAULT_OP_MODE FeistelCipheringOpMode_ECB
+
 // ================= Data structures ===================
 
-// Structure for the Feistel cypher
-typedef struct FeistelCyphering {
+// Operating mode
+typedef enum FeistelCipheringOpMode {
+
+  FeistelCipheringOpMode_ECB,
+  FeistelCipheringOpMode_CBC,
+  FeistelCipheringOpMode_CTR
+
+} FeistelCipheringOpMode;
+
+// Structure for the Feistel cipher
+typedef struct FeistelCiphering {
 
   // GSet of null terminated strings, all the same size,
-  // which are the keys to cypher/decypher
+  // which are the keys to cipher/decipher
   GSetStr* keys;
 
-  // Function to be used during cyphering
-  void (*fun)(
-    unsigned char*,
-    unsigned char*,
-    unsigned char*,
-    unsigned long);
+  // Function to be used during ciphering
+  void FUN_CIPHER;
 
-} FeistelCyphering;
+  // Operating mode
+  FeistelCipheringOpMode mode;
+
+  // Initialization vector as a null terminated string
+  // Used by CBC and CTR operating mode
+  // In case of CTR, space for the counter is allocated at the end of
+  // the initVector given by the user to append the counter
+  unsigned char* initVector;
+
+  // Buffer for the encoding/decoding when using CBC and CTR
+  unsigned char* streamBuffer;
+
+  // Counter for the CTR operating mode
+  unsigned long counter;
+
+} FeistelCiphering;
 
 // ================ Functions declaration ====================
 
-// Static constructor for a Feistel cypher,
+// Static constructor for a Feistel cipher,
 // 'keys' is a GSet of null terminated strings, all the same size
-// 'fun' is the cyphering function of the form
+// 'fun' is the ciphering function of the form
 // void (*fun)(char* src, char* dest, char* key, unsigned long len)
 // 'src', 'dest' have same length 'len'
 // 'key' may be of any length
 #if BUILDMODE != 0
 static inline
 #endif
-FeistelCyphering FeistelCypheringCreateStatic(
+FeistelCiphering FeistelCipheringCreateStatic(
   GSetStr* keys,
-  void (*fun)(
-    unsigned char*,
-    unsigned char*,
-    unsigned char*,
-    unsigned long));
+      void FUN_CIPHER);
 
-// Function to free the memory used by the static FeistelCyphering
-void FeistelCypheringFreeStatic(
-  FeistelCyphering* that);
+// Function to free the memory used by the static FeistelCiphering
+void FeistelCipheringFreeStatic(
+  FeistelCiphering* that);
 
-// Function to cypher the message 'msg' with the FeistelCyphering 'that'
-// The message length 'lenMsg' must be a multiple of the length of
-// the keys
-// Return a new string containing the cyphered message
-unsigned char* FeistelCypheringCypher(
-  FeistelCyphering* that,
-  unsigned char* msg,
-  unsigned long lenMsg);
+// Function to cipher the message 'msg' with the FeistelCiphering 'that'
+// The message length 'lenMsg' must be a multiple of 2
+// Return a new string containing the ciphered message
+unsigned char* FeistelCipheringCipher(
+  FeistelCiphering* that,
+     unsigned char* msg,
+      unsigned long lenMsg);
 
-// Function to decypher the message 'msg' with the FeistelCyphering
+// Function to decipher the message 'msg' with the FeistelCiphering
 // 'that'
-// The message length 'lenMsg' must be a multiple of the length of
-// the keys
-// Return a new string containing the decyphered message
-unsigned char* FeistelCypheringDecypher(
-  FeistelCyphering* that,
-  unsigned char* msg,
-  unsigned long lenMsg);
+// The message length 'lenMsg' must be a multiple of 2
+// Return a new string containing the deciphered message
+unsigned char* FeistelCipheringDecipher(
+  FeistelCiphering* that,
+     unsigned char* msg,
+      unsigned long lenMsg);
+
+// Get the operating mode of the FeistelCiphering 'that'
+#if BUILDMODE != 0
+static inline
+#endif
+FeistelCipheringOpMode FeistelCipheringGetOpMode(
+  const FeistelCiphering* const that);
+
+// Set the operating mode of the FeistelCiphering 'that' to 'mode'
+#if BUILDMODE != 0
+static inline
+#endif
+void FeistelCipheringSetOpMode(
+  FeistelCiphering* const that,
+   FeistelCipheringOpMode mode);
+
+// Get the initialisation vector of the FeistelCiphering 'that'
+#if BUILDMODE != 0
+static inline
+#endif
+const unsigned char* FeistelCipheringGetInitVec(
+  const FeistelCiphering* const that);
+
+// Set the initialisation vector of the FeistelCiphering 'that'
+// to 'initVec'
+// Allocate extra memory to append the counter at the end of the
+// initialisation vector if the operation mode is CTR
+#if BUILDMODE != 0
+static inline
+#endif
+void FeistelCipheringSetInitVec(
+     FeistelCiphering* const that,
+  const unsigned char* const initVec);
+
+// Initialise the stream encoding/decoding of the FeistelCiphering 'that'
+// with the initialization vector 'initVec'
+#if BUILDMODE != 0
+static inline
+#endif
+void FeistelCipheringInitStream(
+     FeistelCiphering* const that,
+  const unsigned char* const initVec);
+
+// Function to cipher a stream of messages 'msg' with the
+// FeistelCiphering 'that'
+// The messages length 'lenMsg' must be a multiple of 2
+// The messages of the 'streamIn' are consumed one after the other
+// and the resulting ciphered messages is appended in the same order
+// to 'streamOut'
+// Memory used by the messages from the 'streamIn' is freed
+// 'lenMsg' must be at least sizeof(that->counter) + 1
+void FeistelCipheringCipherStream(
+    FeistelCiphering* that,
+       GSetStr* const streamIn,
+       GSetStr* const streamOut,
+  const unsigned long lenMsg);
+
+// Function to decipher a stream of messages 'msg' with the
+// FeistelCiphering 'that'
+// The messages length 'lenMsg' must be a multiple of 2
+// The messages of the 'streamIn' are consumed one after the other
+// and the resulting deciphered messages is appended in the same order
+// to 'streamOut'
+// Memory used by the messages from the 'streamIn' is freed
+// 'lenMsg' must be at least sizeof(that->counter) + 1
+void FeistelCipheringDecipherStream(
+    FeistelCiphering* that,
+       GSetStr* const streamIn,
+       GSetStr* const streamOut,
+  const unsigned long lenMsg);
+
+// Get the required size of the initialisation vector for the
+// FeistelCiphering 'that' for messages of length 'lenMsg'
+#if BUILDMODE != 0
+static inline
+#endif
+unsigned long FeistelCipheringGetReqSizeInitVec(
+  const FeistelCiphering* const that,
+            const unsigned long lenMsg);
 
 // ================ inliner ====================
 
